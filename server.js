b1ae86b72b8bbe9455b1a4843d020667c427ece0
Adoniram Josue Vargas <adoniramjvargas@gmail.com>
2019-11-20 11:36:48 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,16 +2,17 @@ const express = require('express')
 const axios = require('axios')
 
 const app = express();
+
 //the urls are for the starship section of swapi
 let url1 = "https://swapi.co/api/starships/?page=1"
 let url2 = "https://swapi.co/api/starships/?page=2"
 let url3 = "https://swapi.co/api/starships/?page=3"
 let url4 = "https://swapi.co/api/starships/?page=4"
 
-
 function fetchURL(url) {
   return axios.get(url)
 }
+
 const promiseArray = [url1, url2, url3, url4].map(fetchURL)
 //promiseArray is an array of promises
 
